78f850e1dd55786022f5d8fbe71f95fc37fadf7f
kowalanumero1 <srramsurrun@gmail.com>
2025-04-29 18:07:44 +0400

    innitial commit

---
+++ vehical class/main.py
@@ -0,0 +1,16 @@
+class vehical :
+
+
+
+    def __init__(self, max_speed, mileage):
+
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+modelX = vehical(240, 10)
+
+
+
+
+print("Model Max Speed:",modelX.max_speed)
+print("Model Mileage:", modelX.mileage)
